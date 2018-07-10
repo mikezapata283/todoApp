826d60fc48fd66980832aa883721f076064a1d46
mike.zapata283@gmail.com <mike.zapata283@gmail.com>
2018-07-10 15:17:26 -0500

    adding todo project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = "Michael";
+  todoInput = '';
+  todos = [];
+// User Inputs Name -> YourName's To Do List
+
+//Creates to do list
+createTodo() {
+  this.todos.push({
+    isChecked: false,
+    name: this.todoInput,
+  });
+  this.todoInput="";
+}
+//Edit to do list
+editTodo(chore) {
+  console.log('Chore that was clicked' + chore);
+  let index = this.todos.indexOf(chore);
+  console.log('Index of that chore:' + index);
+
+
+  this.todos[index].name = prompt('New To Do:');
+}
+
+//Delete to do
+deleteTodo(chore) {
+  console.log('Chore that was clicked' + chore);
+  let index = this.todos.indexOf(chore);
+  this.todos.splice(index, 1);
+  }
 }
