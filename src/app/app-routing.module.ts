3dd044d481f6baa478a8187bffeb7cc37ecb6a0d
mike.zapata283@gmail.com <mike.zapata283@gmail.com>
2018-07-20 14:52:44 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "", 
+    redirectTo: "/",
+    pathMatch: "full"
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+exports: [RouterModule]
+})
+export class AppRoutingModule { }
